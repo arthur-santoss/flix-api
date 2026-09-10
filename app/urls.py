92983681fa76f89c2ce1f6f6545c4497699f60f5
Arthur Santos <arthursantos242568@gmail.com>
2026-09-09 23:30:45 -0300

    Add Actor views and serializer for CRUD operations

--- app/urls.py
+++ app/urls.py
@@ -2,11 +2,16 @@
 from django.contrib import admin
 from django.urls import path
 from genres.views import GenreCreateListView, GenreRetriveUpdateDestroyView
+from actors.views import ActorCreateListView, ActorRetriveUpdateDestroy
 
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+
     path('genres/', GenreCreateListView.as_view(), name='genre_create_list'),
     path('genres/<int:pk>/', GenreRetriveUpdateDestroyView.as_view(), name='genre-detail-view'),
+
+    path('actors/', ActorCreateListView.as_view(), name='actor-create-list'),
+    path('actors/<int:pk>/', ActorRetriveUpdateDestroy.as_view(), name='actor-detail-view')
     
 ]

e53353ee8ec5f14585fd50f824a8c27a106eb53f
Arthur Santos <arthursantos242568@gmail.com>
2026-09-14 22:26:20 -0300

    Refactor URL routing and update binary files
    - Consolidated URL patterns in `urls.py` for actors, genres, movies, and reviews into a single API versioned endpoint.
    - Created separate `urls.py` files for each app (actors, genres, movies, reviews) to manage their respective routes.
    - Updated binary files in `__pycache__` for actors, genres, movies, and reviews due to code changes.

--- app/urls.py
+++ app/urls.py
@@ -1,6 +1,5 @@
 from django.contrib import admin
-from django.urls import path
-from genres.views import GenreCreateListView, GenreRetriveUpdateDestroyView
+from django.urls import path, include
 from actors.views import ActorCreateListView, ActorRetriveUpdateDestroy
 from movies.views import MovieCreateListView, MovieRetrieveUpdateDestroyView
 from reviews.views import ReviewCreateListView, ReviewRetrieveUpdateDestroyView
@@ -9,17 +8,8 @@ from reviews.views import ReviewCreateListView, ReviewRetrieveUpdateDestroyView
 urlpatterns = [
     path('admin/', admin.site.urls),
 
-    path('genres/', GenreCreateListView.as_view(), name='genre_create_list'),
-    path('genres/<int:pk>/', GenreRetriveUpdateDestroyView.as_view(), name='genre-detail-view'),
-
-    path('actors/', ActorCreateListView.as_view(), name='actor-create-list'),
-    path('actors/<int:pk>/', ActorRetriveUpdateDestroy.as_view(), name='actor-detail-view'),
-
-    path('movies/', MovieCreateListView.as_view(), name='movie-create-list'),
-    path('movies/<int:pk>/', MovieRetrieveUpdateDestroyView.as_view(), name='movie-detail-view'),
-
-    path('reviews/', ReviewCreateListView.as_view(), name='revies-create-list'),
-    path('reviews/<int:pk>/', ReviewRetrieveUpdateDestroyView.as_view(), name='review-detail-view')
-
-    
+    path('api/v1/', include('genres.urls')),
+    path('api/v1/', include('actors.urls')),
+    path('api/v1/', include('movies.urls')),
+    path('api/v1/', include('reviews.urls')),    
 ]
